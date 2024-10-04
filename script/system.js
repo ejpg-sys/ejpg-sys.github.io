72435f14670036d5a047ccbc3dd4586e3fb5dcfe
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2024-10-04 16:07:13 -0300

    add improviments

--- script/system.js
+++ script/system.js
@@ -1,5 +1,5 @@
 var system = angular.module("system", []);
-
 system.controller("systemCtrl", function ($scope) {
-  $scope.article = article.json
+  $scope.appname = "hello!";
+  $scope.article = angular.fromJson("article.json");
 });
