5ff7ba8b3371ab2c3b1962ce67351dda4306f84e
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2024-10-09 13:12:25 -0300

    add staged improvements
    --> load json file for tables interation
    --> add ng-show for display content when load data
    --> add progress bar for better user experience

--- script/system.js
+++ script/system.js
@@ -1,5 +1,23 @@
 var system = angular.module("system", []);
 system.controller("systemCtrl", function ($scope) {
-  $scope.appname = "hello!";
-  $scope.article = angular.fromJson("article.json");
+$scope.appname = "hello!";
+/**
+fetch("article.json")
+.then(function(response) {
+  return response.json();
+})
+.then(function(data) {
+  $scope.article = data;
+});
+*/
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'article.json', true);
+xhr.responseType = 'json';
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    var data = xhr.response;
+    $scope.article = data;
+  }
+}
+xhr.send();
 });
