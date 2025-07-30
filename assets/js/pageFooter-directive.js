cb52305c62c2a35a6c26678819b9ce6c5e7cef8a
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2025-07-30 13:22:46 -0300

    page footer context implementation
    add fields data bind values for english and portuguese in directive
    add fields scope data bind in html page
    add context language initializer function in directive

--- assets/js/pageFooter-directive.js
+++ assets/js/pageFooter-directive.js
@@ -18,23 +18,57 @@ system.directive('pageFooter', ['$log', '$http', 'languageService', '$rootScope'
           }
         });
       }
-      var _contextPortuguesLanguage = function() {
-        // about
-        scope.about = "About";
-        scope.team = "Team";
-        scope.locations = "Locations";
-        scope.privacy = "Privacy";
-        scope.terms = "Terms";
-      }
       var _contextEnglishLanguage = function() {
         // about
-        scope.about = "Sobre";
-        scope.team = "Equipe";
-        scope.locations = "Localização";
-        scope.privacy = "Privacidade";
-        scope.terms = "Termos";
+        scope.about = 'About';
+        scope.aboutTeam = 'Team';
+        scope.aboutLocations = 'Locations';
+        scope.aboutPrivacy = 'Privacy';
+        scope.aboutTerms = 'Terms';
+        // blog
+        scope.blog = 'Blog';
+        scope.blogSD = 'Software Developer'
+        // products
+        scope.products = 'Products';
+        scope.productSpecIntepreter = 'Specification Interpreter';
+        scope.productFpaWS = 'FPA WebService';
+        scope.productSysWS = 'Systems WebService';
+        scope.productBaseWS = 'Base WebService';
+        // services
+        scope.services = 'Services';
+        scope.serviceITConsulting = 'IT Consulting';
+        scope.serviceSoftwareManufacture = 'Software Manufacture';
       }
+      var _contextPortuguesLanguage = function() {
+        // about
+        scope.about = "Sobre";
+        scope.aboutTeam = "Equipe";
+        scope.aboutLocations = "Localização";
+        scope.aboutPrivacy = "Privacidade";
+        scope.aboutTerms = "Termos";
+        // blog
+        scope.blog = 'Blog';
+        scope.blogSD = 'Desenvolvimento de Software'
+        // products
+        scope.products = 'Produtos';
+        scope.productSpecIntepreter = 'Interpretador de Especificações';
+        scope.productFpaWS = 'FPA Serviço Web';
+        scope.productSysWS = 'Systems Serviço Web';
+        scope.productBaseWS = 'Base Serviço Web';
+        // services
+        scope.services = 'Serviços';
+        scope.serviceITConsulting = 'Consultoria em TI';
+        scope.serviceSoftwareManufacture = 'Fábrica de Software';
+      }
+      var _initializer = function() {
+        if (languageService.get() === languageService.portugueseLanguage) {
+          _contextPortuguesLanguage();
+		} else if (languageService.get() === languageService.englishLanguage) {
+          _contextEnglishLanguage();
+        }
+	  }
       _languageEventListener();
+      _initializer();
     }
   }
 }]);
