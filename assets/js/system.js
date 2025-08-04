c0a170ae3c022f8115dba14bbb28ac3b6bef7bfe
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2025-08-04 11:57:26 -0300

    page index context implementation
    1. move snap code of dependencies load with requirejs from >
    > dependencie.js to system.js.
    2. change script reference of dependencies.js to system.js in index >
    > page.
    3. remove declaration of angular controller in index page avoiding >
    > unkwon declaration.

--- assets/js/system.js
+++ assets/js/system.js
@@ -3,3 +3,26 @@
  * Copyright (c) 2024-2025 EJPG-SYS
  */
 var system = angular.module("system", []);
+var dependecies = function() {
+  require.config({
+    paths: {
+      'ctrl': '/assets/js/ctrl',
+      'languageService': '/assets/js/language-service',
+      'pageHeaderDirective': '/assets/js/pageHeader-directive',
+      'pageContextDirective': '/assets/js/pageContext-directive',
+      'pageFooterDirective': '/assets/js/pageFooter-directive',
+      'pageBlogDirective': '/assets/js/blog-directive',
+      'pageHomeDirective': '/assets/js/pageHome-directive',
+      'contextService': '/assets/js/context-service'
+    }
+  });
+  require(['languageService']);
+  require(['contextService']);
+  require(['ctrl']);
+  require(['pageHeaderDirective']);
+  require(['pageContextDirective']);
+  require(['pageFooterDirective']);
+  require(['pageBlogDirective']);
+  require(['pageHomeDirective']);
+}
+dependecies();
