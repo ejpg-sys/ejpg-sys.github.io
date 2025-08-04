e365759fb9ef62b53424833359bae44204a8dc9b
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2025-08-04 16:42:46 -0300

    main system mocule context implementation
    1. add indexIdVerify for each resource loader check for >
    > confirm that the async resource has ben loaded done.

--- assets/js/system.js
+++ assets/js/system.js
@@ -4,6 +4,15 @@
  */
 var system = angular.module("system", []);
 system.run(function($rootScope, $compile) {
+  var indexIdVerify01 = false;
+  var indexIdVerify02 = false;
+  var indexIdVerify03 = false;
+  var indexIdVerify04 = false;
+  var indexIdVerify05 = false;
+  var indexIdVerify06 = false;
+  var indexIdVerify07 = false;
+  var indexIdVerify08 = false;
+  var indexIdVerify09 = false;
   var dependecies = function() {
     require.config({
       paths: {
@@ -18,19 +27,20 @@ system.run(function($rootScope, $compile) {
         'developer': '/assets/js/developer-directive'
       }
     });
-    require(['languageService']);
-    require(['contextService']);
-    require(['pageHeaderDirective']);
-    require(['pageContextDirective']);
-    require(['pageFooterDirective']);
-    require(['pageBlogDirective']);
-    require(['pageHomeDirective']);
-    require(['developer']);
-    require(['ctrl'], function(ctrl) {
+    require(['languageService'],function(a){indexIdVerify01=true;});
+    require(['contextService'],function(b){indexIdVerify02=true;});
+    require(['pageHeaderDirective'],function(c){indexIdVerify03=true;});
+    require(['pageContextDirective'],function(d){indexIdVerify04=true;});
+    require(['pageFooterDirective'],function(e){indexIdVerify05=true;});
+    require(['pageBlogDirective'],function(f){indexIdVerify06=true;});
+    require(['pageHomeDirective'],function(g){indexIdVerify07=true;});
+    require(['developer'],function(h){indexIdVerify08=true;});
+    require(['ctrl'], function(j) {
+      indexIdVerify09 = true;
       document.querySelector('body').setAttribute('ng-controller', 'ctrl');
+      $rootScope.$apply();
+      $rootScope.$digest();
     });
-    $rootScope.$apply();
-    $rootScope.$digest();
   }
   dependecies();
 });
