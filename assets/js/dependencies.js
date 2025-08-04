a2facfdc6112e0ea2f7c1a8a2082b2b1e54488e7
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2025-08-04 11:08:04 -0300

    effective clean code from startegies interactions

--- assets/js/dependencies.js
+++ assets/js/dependencies.js
@@ -20,18 +20,6 @@ window.addEventListener('load', (event) => {
     developerInfo('is ready!');
   }
 });
-var dependencies = [
-{ name: "Bootstrap Carousel", type: "css", addr: "assets/css/bootstrap-carousel.css" },
-{ name: "Bootstrap Breadcrumbs", type: "css", addr: "assets/css/bootstrap-breadcrumbs.css" },
-{ name: "AngularJS System Module", type: "js", addr: "assets/js/system.js?v=1234567890" },
-{ name: "AngularJS Language Service", type: "js", addr: "assets/js/language-service.js?v=1234567890" },
-{ name: "AngularJS Page Header Directive", type: "js", addr: "assets/js/pageHeader-directive.js?v=1234567890" },
-{ name: "AngularJS Page Context Directive", type: "js", addr: "assets/js/pageContext-directive.js?v=1234567890" },
-{ name: "AngularJS Page Footer Directive", type: "js", addr: "assets/js/pageFooter-directive.js?v=1234567890" },
-{ name: "AngularJS Blog Page Directive", type: "js", addr: "assets/js/blog-directive.js?v=1234567890" },
-{ name: "AngularJS Home Page Directive", type: "js", addr: "assets/js/pageHome-directive.js?v=1234567890" },
-{ name: "AngularJS Context Service", type: "js", addr: "assets/js/context-service.js?v=1234567890" }
-];
 var requireDependencies = function() {
   require.config({
     paths: {
@@ -44,17 +32,9 @@ var requireDependencies = function() {
       'pageBlogDirective': '/assets/js/blog-directive',
       'pageHomeDirective': '/assets/js/pageHome-directive',
       'contextService': '/assets/js/context-service'
-    },
-    async: false,
-    waitSeconds: 0,
-    priority: ['system'],
-    callback: function() {
-      console.log('ready!');
-	},
-    deps: ['languageService','contextService','ctrl','pageHeaderDirective',
-	'pageContextDirective','pageFooterDirective','pageBlogDirective','pageHomeDirective']
+    }
   });
-/*  require(['system'], function() {
+  require(['system'], function() {
     require(['languageService']);
     require(['contextService']);
     require(['ctrl']);
@@ -64,6 +44,6 @@ var requireDependencies = function() {
     require(['pageBlogDirective']);
     require(['pageHomeDirective']);
     dependenciesReady=true;
-  }); */
+  });
 }
-var complete = requireDependencies();
+requireDependencies();
