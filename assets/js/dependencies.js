9f4436f41d68aa8061c4a827b443e9bdca469823
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2025-08-04 08:53:07 -0300

    page index context implementation
    move dependencies declaration one per one from body >
    > to script dependencies declaration.

---
+++ assets/js/dependencies.js
@@ -0,0 +1,18 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2024-2025 EJPG-SYS
+ */
+var dependenciesReady = false;
+var dependencies = [
+{ name: "Bootstrap Carousel" type: "css" addr: "assets/css/bootstrap-carousel.css" },
+{ name: "Bootstrap Breadcrumbs" type: "css" addr: "assets/css/bootstrap-breadcrumbs.css" },
+{ name: "AngularJS System Module" type: "js", addr: "assets/js/system.js?v=1234567890" },
+{ name: "AngularJS Language Service" type: "js", addr: "assets/js/language-service.js?v=1234567890" },
+{ name: "AngularJS Page Header Directive" type: "js", addr: "assets/js/pageHeader-directive.js?v=1234567890" },
+{ name: "AngularJS Page Context Directive" type: "js", addr: "assets/js/pageContext-directive.js?v=1234567890" },
+{ name: "AngularJS Page Footer Directive" type: "js", addr: "assets/js/pageFooter-directive.js?v=1234567890" },
+{ name: "AngularJS Blog Page Directive" type: "js", addr: "assets/js/blog-directive.js?v=1234567890" },
+{ name: "AngularJS Home Page Directive" type: "js", addr: "assets/js/pageHome-directive.js?v=1234567890" },
+{ name: "AngularJS Context Service" type: "js", addr: "assets/js/context-service.js?v=1234567890" }
+];
+
