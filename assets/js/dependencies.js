7e5f691776d976dc420405fc538ef5c0d1a6e00e
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2025-08-05 10:01:05 -0300

    main system module context implementation
    1. move context load declaration from system.js to dependencies.js.

---
+++ assets/js/dependencies.js
@@ -0,0 +1,29 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2024-2025 EJPG-SYS
+ */
+var statusIndex = [false, false, false, false, false, false, false, false, false, false];
+require.config({
+  paths: {
+    'system': '/assets/js/system',
+    'ctrl': '/assets/js/ctrl',
+    'languageService': '/assets/js/language-service',
+    'pageHeaderDirective': '/assets/js/pageHeader-directive',
+    'pageContextDirective': '/assets/js/pageContext-directive',
+    'pageFooterDirective': '/assets/js/pageFooter-directive',
+    'pageBlogDirective': '/assets/js/blog-directive',
+    'pageHomeDirective': '/assets/js/pageHome-directive',
+    'contextService': '/assets/js/context-service',
+    'developer': '/assets/js/developer-directive'
+  }
+});
+require(['system'],function(){statusIndex[0]=true;});
+require(['languageService'],function(){statusIndex[1]=true;});
+require(['contextService'],function(){statusIndex[2]=true;});
+require(['pageHeaderDirective'],function(){statusIndex[3]=true;});
+require(['pageContextDirective'],function(){statusIndex[4]=true;});
+require(['pageFooterDirective'],function(){statusIndex[5]=true;});
+require(['pageBlogDirective'],function(){statusIndex[6]=true;});
+require(['pageHomeDirective'],function(){statusIndex[7]=true;});
+require(['developer'],function(){statusIndex[8]=true;});
+require(['ctrl'],function(){statusIndex[9] = true;});
--- assets/js/system.js
+++ assets/js/system.js
@@ -2,30 +2,4 @@
  * The MIT License (MIT)
  * Copyright (c) 2024-2025 EJPG-SYS
  */
-var statusIndex = [false, false, false, false, false, false, false, false, false];
-var dependecies = function() {
-  require.config({
-    paths: {
-      'ctrl': '/assets/js/ctrl',
-      'languageService': '/assets/js/language-service',
-      'pageHeaderDirective': '/assets/js/pageHeader-directive',
-      'pageContextDirective': '/assets/js/pageContext-directive',
-      'pageFooterDirective': '/assets/js/pageFooter-directive',
-      'pageBlogDirective': '/assets/js/blog-directive',
-      'pageHomeDirective': '/assets/js/pageHome-directive',
-      'contextService': '/assets/js/context-service',
-      'developer': '/assets/js/developer-directive'
-    }
-  });
-  require(['languageService'],function(a){statusIndex[0]=true;});
-  require(['contextService'],function(b){statusIndex[1]=true;});
-  require(['pageHeaderDirective'],function(c){statusIndex[2]=true;});
-  require(['pageContextDirective'],function(d){statusIndex[3]=true;});
-  require(['pageFooterDirective'],function(e){statusIndex[4]=true;});
-  require(['pageBlogDirective'],function(f){statusIndex[5]=true;});
-  require(['pageHomeDirective'],function(g){statusIndex[6]=true;});
-  require(['developer'],function(h){statusIndex[7]=true;});
-  require(['ctrl'],function(j){statusIndex[8] = true;});
-}
-dependecies();
 var system = angular.module("system", []);
