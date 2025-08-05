2db6584d62c6add1856d6f4c137562814e7faea4
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2025-08-05 11:28:58 -0300

    dependecies system module context implementation
    1. reorganize declaration of module require by inline order priority >
    > with require function, change contexts check to function callback >
    > ready, add variable to while loop but this does not work as spected >
    > from callbacks because the state o variable comming from outside is >
    > blocked by the loop context, not effective solution, but improved >
    > design.

--- assets/js/dependencies.js
+++ assets/js/dependencies.js
@@ -2,7 +2,7 @@
  * The MIT License (MIT)
  * Copyright (c) 2024-2025 EJPG-SYS
  */
-var statusIndex = [false, false, false, false, false, false, false, false, false, false];
+var indexReady = false;
 require.config({
   paths: {
     'system': '/assets/js/system',
@@ -17,16 +17,20 @@ require.config({
     'developer': '/assets/js/developer-directive'
   }
 });
-require(['system'],function(){statusIndex[0]=true;});
-require(['languageService'],function(){statusIndex[1]=true;});
-require(['contextService'],function(){statusIndex[2]=true;});
-require(['pageHeaderDirective'],function(){statusIndex[3]=true;});
-require(['pageContextDirective'],function(){statusIndex[4]=true;});
-require(['pageFooterDirective'],function(){statusIndex[5]=true;});
-require(['pageBlogDirective'],function(){statusIndex[6]=true;});
-require(['pageHomeDirective'],function(){statusIndex[7]=true;});
-require(['developer'],function(){statusIndex[8]=true;});
-require(['ctrl'],function(){statusIndex[9] = true;});
+require([
+  'system',
+  'languageService',
+  'contextService',
+  'pageHeaderDirective',
+  'pageContextDirective',
+  'pageFooterDirective',
+  'pageBlogDirective',
+  'pageHomeDirective',
+  'developer',
+  'ctrl'
+], function() {
+  indexReady = true;
+});
 console.log('Not ready! ' + Date.now());
-while (true) {}
+while (!indexReady) {}
 console.log('Ready! ' + Date.now());
