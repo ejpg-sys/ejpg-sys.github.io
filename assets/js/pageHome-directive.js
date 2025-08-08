3be61eeff6c1d6e89b0f0d748cecee59d3519c2b
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2025-08-08 14:49:52 -0300

    resources context design implementation
    1. normalize language patterns in portuguese and english, edited >
    > in page home directive js.

--- assets/js/pageHome-directive.js
+++ assets/js/pageHome-directive.js
@@ -21,11 +21,11 @@ system.directive('pageHome', ['$log', 'languageService', '$rootScope', function(
       var _contextEnglishLanguage = function() {
         // carousel
         scope.service1Title = 'Databases Development';
-        scope.service1Summary = 'Databases Development by Custon-Built.';
+        scope.service1Summary = 'Databases Development Custon-Built.';
         scope.service2Title = 'WebServices Development';
-        scope.service2Summary = 'WebServices Development by Custon-Built.';
+        scope.service2Summary = 'WebServices Development Custon-Built.';
         scope.service3Title = 'Web Front-End Development';
-        scope.service3Summary = 'Web Front-End Development by Custon-Built.';
+        scope.service3Summary = 'Web Front-End Development Custon-Built.';
         scope.service4Title = 'IT Consulting';
         scope.service4Summary = 'Business support';
         // markegin insight
@@ -38,22 +38,22 @@ system.directive('pageHome', ['$log', 'languageService', '$rootScope', function(
         // highlight
         scope.highlight1Title = 'Databases';
         scope.highlight1SubTitle = 'Modeling and Implementation of ILF.';
-        scope.highlight1Text = 'Design and Data Solution by Custon-Built.';
+        scope.highlight1Text = 'Design and Data Solution Custon-Built.';
         scope.highlight2Title = 'WebServices';
         scope.highlight2SubTitle = 'Integration and Data Processing by ILF, EIF, EQ, EI, and EO.';
-        scope.highlight2Text = 'Desing and Solution of WebServices by Custon-Built.';
+        scope.highlight2Text = 'Desing and Solution of WebServices Custon-Built.';
         scope.highlight3Title = 'Front-End Web';
         scope.highlight3SubTitle = 'Integration and Data Processing by EQ, EI, and EO.';
-        scope.highlight3Text = 'Desing and Solution of Front-End Web by Custon-Built.';
+        scope.highlight3Text = 'Desing and Solution of Front-End Web Custon-Built.';
       }
       var _contextPortuguesLanguage = function() {
         // carousel
         scope.service1Title = 'Desenvolvimento de Banco de Dados';
-        scope.service1Summary = 'Desenvolvimento de Banco de Dados Sob-Encomenda.'
+        scope.service1Summary = 'Desenvolvimento de Banco de Dados Sob Encomenda.'
         scope.service2Title = 'Desenvolvimento de Serviços Web';
-        scope.service2Summary = 'Desenvolvimento de Serviços Web Sob-Encomenda.';
+        scope.service2Summary = 'Desenvolvimento de Serviços Web Sob Encomenda.';
         scope.service3Title = 'Desenvolvimento de Front-End Web';
-        scope.service3Summary = 'Desenvolvimento de Front-End Web Sob-Encomenda.';
+        scope.service3Summary = 'Desenvolvimento de Front-End Web Sob Encomenda.';
         scope.service4Title = 'Consultoria em TI';
         scope.service4Summary = 'Apoio comercial';
         // markegin insight
@@ -66,13 +66,13 @@ system.directive('pageHome', ['$log', 'languageService', '$rootScope', function(
         // highlight
         scope.highlight1Title = 'Banco de Dados';
         scope.highlight1SubTitle = 'Modelagem e Implementação de ALI.';
-        scope.highlight1Text = 'Desenho e Solução de Dados Sob-Encomenda.';
+        scope.highlight1Text = 'Desenho e Solução de Dados Sob Encomenda.';
         scope.highlight2Title = 'Serviços Web';
         scope.highlight2SubTitle = 'Integrações e Processamento de Dados por ALI, AIE, CE, EE, e SE.';
-        scope.highlight2Text = 'Desenho e Solução de Serviços Web Sob-Encomenda.';
+        scope.highlight2Text = 'Desenho e Solução de Serviços Web Sob Encomenda.';
         scope.highlight3Title = 'Front-End Web';
         scope.highlight3SubTitle = 'Integrações e Processamento de Dados por CE, EE, e SE.';
-        scope.highlight3Text = 'Desenho e Soluções de Front-End Web Sob-Encomenda.';
+        scope.highlight3Text = 'Desenho e Solução de Front-End Web Sob Encomenda.';
       }
       var _initializer = function() {
         if (languageService.get() === languageService.portugueseLanguage) {
