c6f0f8df06432ec96578140f63036158566d9bb0
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2025-08-08 10:11:00 -0300

    resources context design implementation
    1. move implementation from controller definition to context >
    > directive definition.

--- assets/js/system.js
+++ assets/js/system.js
@@ -3,32 +3,6 @@
  * Copyright (c) 2024-2025 EJPG-SYS
  */
 var system = angular.module("system", []);
-system.controller("ctrl", function ($scope, $http, $log, languageService, contextService, $rootScope) {
+system.controller("ctrl", function ($scope, $http, $log, languageService, $rootScope) {
   languageService.userLanguagePreference(undefined);
-  var _setHomeContextActive = function() {
-    $scope.isHomeContextActive = true;
-    $scope.isBlogContextActive = false;
-  }
-  var _setBlogContextActive = function() {
-    $scope.isBlogContextActive = true;
-    $scope.isHomeContextActive = false;
-  }
-  var _contextEventListener = function() {
-    $rootScope.$on('contextEvent', function(event, data) {
-      if (contextService.pageHome === data) {
-        _setHomeContextActive();
-      } else if (contextService.pageBlog === data) {
-        _setBlogContextActive();
-      }
-    });
-  }
-  _contextEventListener();
-  var _initializer = function() {
-    if (contextService.get() === contextService.pageHome) {
-      _setHomeContextActive();
-    } else if (contextService.get() === contextService.pageBlog) {
-      _setBlogContextActive();
-    }
-  }
-  _initializer();
 });
